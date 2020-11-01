f607e759f48a6454f5cf65bfffe9181305020b7f
afiaka87 <claymullis@fastmail.com>
2020-11-01 02:43:29 -0600

    Revert "Merge updates"

--- log.js
+++ log.js
@@ -1,21 +1,8 @@
 let lines = [];
 
-export const v = (...args) => record('V', args);
 export const i = (...args) => record('I', args);
 export const w = (...args) => record('W', args);
 export const e = (...args) => record('E', args);
-
-window.addEventListener('error', (message, src, row, col, error) => {
-  e(message);
-  v(error);
-});
-
-window.addEventListener('unhandledrejection', event => {
-  let error = event.reason;
-  e(error.message);
-  v(error);
-  event.preventDefault();
-});
 
 export function download() {
   let blob = new Blob([lines.join('\n')], { type: 'text/plain' });
@@ -26,10 +13,9 @@ export function download() {
 }
 
 function record(level, args) {
-  let time = new Date().toJSON().replace(/^.+T|Z$/g, '');
+  let time = new Date().toJSON();
   let line = [time, ...args].join(' ');
   lines.push(line);
-  level == 'V' && console.debug(...args);
   level == 'I' && console.info(...args);
   level == 'W' && console.warn(...args);
   level == 'E' && console.error(...args);
